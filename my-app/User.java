f37798b75d0356ee1f580c16e186cec02c994acc
wboulton <wboulton@purdue.edu>
2025-01-09 14:59:19 -0500

    Create User.java

---
+++ my-app/User.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class User {
+    private ArrayList<String> wishList;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String password;
+}
