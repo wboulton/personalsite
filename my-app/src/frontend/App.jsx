5a38916895d16d0775bcdee7d8b3744eb8746681
wboulton <wboulton@purdue.edu>
2025-01-23 16:21:28 -0500

    prepping to implement node backend

--- my-app/src/frontend/App.jsx
+++ my-app/src/frontend/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
-import reactLogo from './assets/react.svg';
+import reactLogo from '../assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import ReactMarkdown from 'react-markdown';
